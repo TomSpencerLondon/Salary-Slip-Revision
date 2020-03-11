cd185789995d1f1f86701d59891642a6a5363474
Tom Spencer <tom@homeflow.co.uk>
2020-03-11 21:45:04 +0000

    Add getTaxPayable, getTaxableIncome methods and tests

--- src/main/java/com/codurance/salary_slip/SalarySlip.java
+++ src/main/java/com/codurance/salary_slip/SalarySlip.java
@@ -2,6 +2,12 @@ package com.codurance.salary_slip;
 
 public class SalarySlip {
 
+  public static final int MINIMUM_SALARY_FOR_NATIONAL_INSURANCE = 8060;
+  public static final int MONTHS_IN_YEAR = 12;
+  public static final double NATIONAL_INSURANCE_PERCENTAGE = 0.12;
+  public static final double TAXABLE_PERCENTAGE = 0.20;
+  public static final double GROSS_TAX_FREE_ALLOWANCE = 11000.0;
+  public static final double ROUNDING_ADJUSTMENT = 100.0;
   private final String name;
   private final double salary;
 
@@ -12,5 +18,24 @@ public class SalarySlip {
 
   public double getMonthlySalary() {
     return Math.floor(salary / 12);
+  }
+
+  public double getNationalInsuranceContribution() {
+    double result = salary - MINIMUM_SALARY_FOR_NATIONAL_INSURANCE;
+    result = result * NATIONAL_INSURANCE_PERCENTAGE;
+    return Math.floor(result / MONTHS_IN_YEAR);
+  }
+
+  public double getMonthlyTaxFreeAllowance(){
+    double monthlyTaxFeeAllowance = GROSS_TAX_FREE_ALLOWANCE / MONTHS_IN_YEAR;
+    return Math.round(monthlyTaxFeeAllowance * ROUNDING_ADJUSTMENT) / ROUNDING_ADJUSTMENT;
+  }
+
+  public double getTaxableIncome(){
+    return getMonthlySalary() - getMonthlyTaxFreeAllowance();
+  }
+
+  public double getTaxPayable(){
+    return getTaxableIncome() * TAXABLE_PERCENTAGE;
   }
 }
--- src/test/java/com/codurance/salary_slip/SalarySlipGeneratorShould.java
+++ src/test/java/com/codurance/salary_slip/SalarySlipGeneratorShould.java
@@ -17,4 +17,54 @@ public class SalarySlipGeneratorShould {
     SalarySlip result = salarySlipGenerator.generateFor(employee);
     Assert.assertEquals(monthlySalary, result.getMonthlySalary(), 0);
   }
+
+  @Test
+  public void create_slip_for_employee_with_national_insurance() {
+    int id = 12345;
+    String name = "John J Doe";
+    double annualGrossSalary = 9060;
+    Employee employee = new Employee(id, name, annualGrossSalary);
+    SalarySlipGenerator salarySlipGenerator = new SalarySlipGenerator();
+    double nationalInsuranceContribution = 10;
+
+    SalarySlip result = salarySlipGenerator.generateFor(employee);
+    Assert.assertEquals(nationalInsuranceContribution, result.getNationalInsuranceContribution(), 0);
+  }
+
+
+  @Test
+  public void create_slip_for_employee_with_taxable_income() {
+    int id = 12345;
+    String name = "John J Doe";
+    double annualGrossSalary = 12000;
+    Employee employee = new Employee(id, name, annualGrossSalary);
+    SalarySlipGenerator salarySlipGenerator = new SalarySlipGenerator();
+
+    double nationalInsuranceContribution = 10;
+
+    SalarySlip result = salarySlipGenerator.generateFor(employee);
+
+
+    Assert.assertEquals(916.67, result.getMonthlyTaxFreeAllowance(), 0.1);
+    Assert.assertEquals(83.33, result.getTaxableIncome(), 0.1);
+    Assert.assertEquals(16.67, result.getTaxPayable(), 0.1);
+  }
+
+  @Test
+  public void create_slip_for_employee_with_gross_salary_of_45_000() {
+    int id = 12345;
+    String name = "John J Doe";
+    double annualGrossSalary = 45000;
+    Employee employee = new Employee(id, name, annualGrossSalary);
+    SalarySlipGenerator salarySlipGenerator = new SalarySlipGenerator();
+
+    double nationalInsuranceContribution = 10;
+
+    SalarySlip result = salarySlipGenerator.generateFor(employee);
+
+
+    Assert.assertEquals(916.67, result.getMonthlyTaxFreeAllowance(), 0.1);
+    Assert.assertEquals(83.33, result.getTaxableIncome(), 0.1);
+    Assert.assertEquals(16.67, result.getTaxPayable(), 0.1);
+  }
 }
